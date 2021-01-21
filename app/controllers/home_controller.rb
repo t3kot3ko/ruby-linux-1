2278427c159abae2cbdeda18fc1a2f1966effcd0
Ubuntu <edisga@edisga-AzureOSSLinuxVM.kmrxw2kq3dwe5d0cxioex2swyc.jx.internal.cloudapp.net>
2021-01-21 09:14:08 +0000

    Initial Commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,26 @@
+class DirectoryService
+
+        def Search
+
+		directories = ""
+		tmp = Dir.glob('/home/edisga/**/*')
+
+		tmp.each do |file_name|
+			#p file_name
+			directories = directories + "-" + file_name 
+		end
+	
+		return tmp
+        end
+end
+
 class HomeController < ApplicationController
-  include Directory
+
+  $directoriesArray ||= Array.new
 
   def index
-    Directory.Search
-    @output = "Response back from method in #{elapsed_seconds}"
+    service = DirectoryService.new
+    $directoriesArray.push(service.Search)
+    @output = "Response back from method"
   end
 end
--- lib/directory.rb
+++
@@ -1,9 +0,0 @@
-module Directory
-    def Search
-        Dir.foreach('C:\temp') do |filename|
-            next if filename == '.' or filename == '..'
-            Rails.logger.info filename
-            # Do work on the remaining files & directories
-        end
-    end
-end
